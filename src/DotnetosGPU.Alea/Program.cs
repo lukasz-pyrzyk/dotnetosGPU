c96bd5a64c69b97058ea1eee0708c67f1ad4a0b5
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2018-03-11 08:42:58 +0100

    Added alea project

---
+++ src/DotnetosGPU.Alea/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Alea;
+using Alea.CSharp;
+using Alea.Parallel;
+
+namespace DotnetosGPU.Alea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action action;
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Missing command");
+                return;
+            }
+
+            switch (args[0])
+            {
+                case "gpu":
+                    action = RunGpu;
+                    break;
+                default:
+                    Console.WriteLine($"Missing command. Expecting gpu | cpu");
+                    return;
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Warm up {i}/10");
+                action?.Invoke();
+            }
+
+            GC.Collect(3);
+            GC.WaitForPendingFinalizers();
+            GC.Collect(3);
+
+            var watch = Stopwatch.StartNew();
+            action();
+            watch.Stop();
+            Console.WriteLine($"Done in {watch.Elapsed}");
+        }
+
+        private static void RunGpu()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static int GetData(out float[] x, out float[] y)
+        {
+            int n = 10_000_000;
+            x = Enumerable.Range(0, n).Select(i => i + 0.3f).ToArray();
+            y = Enumerable.Range(0, n).Select(i => i + 0.6f).ToArray();
+
+            return n;
+        }
+    }
+}
